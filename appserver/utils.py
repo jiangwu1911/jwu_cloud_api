c48e9b234873738c782ac0b40001d1bbc7345d42
wujiang <jiangwu1911@hotmail.com>
2014-08-03 18:57:33 +0800

    Json encode for sql results

--- appserver/utils.py
+++ appserver/utils.py
@@ -1,31 +1,51 @@
 import uuid
 import json
+from sqlalchemy.ext.declarative import DeclarativeMeta  
+
 
 def get_uuid():
     return uuid.uuid4().hex
 
 
-class JsonEncoder(json.JSONEncoder):
-    def default(self,obj):
-        d = {}
-        d[obj.__class__.__name__] = {}
-        d[obj.__class__.__name__].update(obj.__dict__)
-        return d
+def sql_results_to_json(results, name):
+    items = []
+    for item in results:
+        items.append(item)
+    return '{"' + name + '": ' + json_dumps(items) + '}'
 
 
-class JsonDecoder(json.JSONDecoder):
-    def __init__(self):
-        json.JSONDecoder.__init__(self,object_hook=self.dict2object)
+def json_dumps(obj):
+    return json.dumps(obj, cls=new_alchemy_encoder(), check_circular=False)
 
-    def dict2object(self,d):
-        if'__class__' in d:
-            class_name = d.pop('__class__')
-            module_name = d.pop('__module__')
-            module = __import__(module_name)
-            class_ = getattr(module,class_name)
-            args = dict((key.encode('ascii'), value) for key, value in d.items()) 
-            inst = class_(**args) 
-        else:
-            inst = d
-        return inst
 
+def new_alchemy_encoder(revisit_self = False, fields_to_expand = []):
+    _visited_objs = []
+    class AlchemyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj.__class__, DeclarativeMeta):
+                # don't re-visit self
+                if revisit_self:
+                    if obj in _visited_objs:
+                        return None
+                    _visited_objs.append(obj)
+
+                # go through each field in this SQLalchemy class
+                fields = {}
+                for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                    val = obj.__getattribute__(field)
+
+                    # is this field another SQLalchemy object, or a list of SQLalchemy objects?
+                    if isinstance(val.__class__, DeclarativeMeta) or (isinstance(val, list) 
+                            and len(val) > 0 and isinstance(val[0].__class__, DeclarativeMeta)):
+                        # unless we're expanding this field, stop here
+                        if field not in fields_to_expand:
+                            # not expanding this field: set it to None and continue
+                            fields[field] = None
+                            continue
+
+                    fields[field] = val
+                # a json-encodable dict
+                return fields
+
+            return json.JSONEncoder.default(self, obj)
+    return AlchemyEncoder
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,18 +1,19 @@
 import sys
 import unittest
+import json
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 sys.path.append("..")
 from appserver.model import User
-from appserver.utils import JsonEncoder
+from appserver.utils import sql_results_to_json
 
 
 class JsonTestCase(unittest.TestCase):
     def _create_session(self):
         engine = create_engine('mysql://%s:%s@%s/%s?charset=%s' %
                                ('root', '', 'localhost', 'cloudapi', 'utf8'),
-                               echo=False)
+                               echo=True)
         Session = sessionmaker(bind=engine)
         session = Session()
         return session
@@ -20,11 +21,9 @@ class JsonTestCase(unittest.TestCase):
 
     def test_encode(self):
         session = self._create_session()
-        users = session.query(User)
-        #for user in users:
-        #    print JsonEncoder().encode(user)
-        #user = User(name='admin', password='admin')
-        #print JsonEncoder().encode(user)
+        results = session.query(User)
+        str = sql_results_to_json(results, 'users')
+        print json.loads(str) 
         
 
 if __name__ == "__main__":
