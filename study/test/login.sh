499d0240cd6e5989e808065cf57bd6f669a4e783
wujiang <jiangwu1911@hotmail.com>
2014-08-03 19:27:06 +0800

    Removed folder 'study'

--- study/test/login.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -X POST -d 'username=admin&password=admin' http://127.0.0.1:8080/login
--- study/test/threads.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for i in $(seq 100)  ; do
-    wget http://127.0.0.1:8080/hello --delete-after &
-done
